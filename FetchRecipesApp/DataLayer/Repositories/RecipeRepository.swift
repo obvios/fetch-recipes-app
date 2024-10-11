6057bb4105a588e45eab340a6a2798b243e13324
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-11 13:19:30 -0400

    add use case impl and recipe repo

---
+++ FetchRecipesApp/DataLayer/Repositories/RecipeRepository.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeRepository.swift
+//  FetchRecipesApp
+//
+//  Created by Eric Palma on 10/11/24.
+//
+
+import Foundation
---
+++ FetchRecipesApp/Domain/UseCases/FetchRecipesUseCaseImpl.swift
@@ -0,0 +1,8 @@
+//
+//  FetchRecipesUseCaseImpl.swift
+//  FetchRecipesApp
+//
+//  Created by Eric Palma on 10/11/24.
+//
+
+import Foundation
