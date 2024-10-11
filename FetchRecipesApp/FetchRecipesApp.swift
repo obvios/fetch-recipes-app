f16c27e9970f8eb1727580521be313279013c414
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-11 12:14:02 -0400

    init commit

---
+++ FetchRecipesApp/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipesApp
+//
+//  Created by Eric Palma on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
