4dcef66f1608ef2ef5ce5cdd32b31835a0f0a4f0
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-11 14:17:03 -0400

    Implement RecipeRepo

--- FetchRecipesApp/DataLayer/Repositories/RecipeRepository.swift
+++ FetchRecipesApp/DataLayer/Repositories/RecipeRepository.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class RecipeRepository: RecipeRepositoryProtocol {
+    private let apiClient: RecipeAPIClient
+    
+    init(apiClient: RecipeAPIClient) {
+        self.apiClient = apiClient
+    }
+    
+    func fetchRecipes() async throws -> [Recipe] {
+        do {
+            let data = try await apiClient.fetchRecipeData()
+            let decoder = JSONDecoder()
+            let recipeDTOs = try decoder.decode([RecipeDTO].self, from: data)
+            return recipeDTOs.map { Recipe(from: $0) }
+        } catch let error as RecipeAPIError {
+            // Pass along RecipeAPIError directly to the caller
+            throw error
+        } catch {
+            // Catch any other errors as decoding issues
+            throw RecipeRepositoryError.malformedData
+        }
+    }
+}
