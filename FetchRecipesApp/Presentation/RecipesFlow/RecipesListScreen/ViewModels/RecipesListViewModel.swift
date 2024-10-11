4450f434699d1553e9e66c19f079fad1cb339ace
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-11 19:02:31 -0400

    create recipes list view model

--- FetchRecipesApp/Presentation/RecipesFlow/RecipesListScreen/ViewModels/RecipesListViewModel.swift
+++ FetchRecipesApp/Presentation/RecipesFlow/RecipesListScreen/ViewModels/RecipesListViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+@MainActor
+class RecipesListViewModel: ObservableObject {
+    // MARK: - Published Properties (UI States)
+    @Published var recipes: [Recipe] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String? = nil
+    
+    private let fetchRecipesUseCase: FetchRecipesUseCase
+    
+    // MARK: - Initializer
+    init(fetchRecipesUseCase: FetchRecipesUseCase) {
+        self.fetchRecipesUseCase = fetchRecipesUseCase
+    }
+    
+    // MARK: - Methods
+    func loadRecipes() async {
+        do {
+            isLoading = true
+            errorMessage = nil
+            
+            let recipes = try await fetchRecipesUseCase.execute()
+            // Check if data is empty, update UI state accordingly
+            if recipes.isEmpty {
+                errorMessage = "No recipes available."
+            } else {
+                self.recipes = recipes
+            }
+            isLoading = false
+        } catch FetchRecipesUseCaseError.emptyData {
+            errorMessage = "No recipes available."
+            isLoading = false
+        } catch {
+            // Handle any other unexpected errors
+            errorMessage = "An unexpected error occurred. Please try again."
+            isLoading = false
+        }
+    }
+}
