d883ecbbf8f75698914d15c5613b74039b1cfb33
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-12 17:57:40 -0400

    add recipe row view file

---
+++ FetchRecipesApp/Presentation/RecipesFlow/RecipesListScreen/Views/RecipeRowView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeRowView.swift
+//  FetchRecipesApp
+//
+//  Created by Eric Palma on 10/12/24.
+//
+
+import SwiftUI
+
+struct RecipeRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeRowView()
+}
