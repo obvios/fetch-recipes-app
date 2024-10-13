95a55992fd7359cf693e340e88384df17264179f
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-13 10:41:06 -0400

    add recipes list view

---
+++ FetchRecipesApp/Presentation/RecipesFlow/RecipesListScreen/Views/RecipeListView.swift
@@ -0,0 +1,44 @@
+//
+//  RecipeListView.swift
+//  FetchRecipesApp
+//
+//  Created by Eric Palma on 10/13/24.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    @StateObject private var viewModel: RecipesListViewModel
+        
+    init(viewModel: RecipesListViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        VStack {
+            if viewModel.isLoading {
+                ProgressView("Loading recipes...")
+                    .padding()
+            } else if let errorMessage = viewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            } else {
+                List(viewModel.recipes) { recipe in
+                    RecipeRowView(recipe: recipe)
+                }
+                .listStyle(PlainListStyle())
+                .refreshable {
+                    await viewModel.loadRecipes()
+                }
+            }
+        }
+        .navigationTitle("Recipes")
+        .onAppear {
+            Task {
+                await viewModel.loadRecipes()
+            }
+        }
+    }
+}
