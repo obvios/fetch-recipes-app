a09c4f622ffa049f98294739139a6f032c9fbcb3
Eric Palma <ericpalma@Erics-Mac-mini.local>
2024-10-13 11:12:49 -0400

    add response object for api containing recipes

--- FetchRecipesApp/DataLayer/API/RecipeDTO.swift
+++ FetchRecipesApp/DataLayer/API/RecipeDTO.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
+/// DTO struct representing an api response for Recipes
+struct RecipesDTO: Codable {
+    let recipes: [RecipeDTO]
+}
+
+/// DTO struct representing a single Recipe
 struct RecipeDTO: Codable {
     let cuisine: String
     let name: String
     let photo_url_large: String
     let photo_url_small: String
-    let source_url: String
+    let source_url: String?
     let uuid: String
-    let youtube_url: String
+    let youtube_url: String?
 }
